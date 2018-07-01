81be06412135b1a19d8b61ddf243794507646acd
Antiarisxu <jimmyxurs@gmail.com>
2018-07-01 12:10:53 +0800

    Update util.java

--- PID Drive/src/org/usfirst/frc/team6394/robot/util.java
+++ PID Drive/src/org/usfirst/frc/team6394/robot/util.java
@@ -4,4 +4,16 @@ public abstract class util {
 	public static double deadband(double val, double threshold) {
 		return Math.abs(val)>=threshold?val:0;
 	}
+	
+	public static double equalsign(double sign, double value) {
+		return Math.abs(value)*Math.abs(sign)/sign;
+	}
+	
+	public static boolean isWithin(double value, double center, double range) {
+		/*** 
+		 * range is in percent
+		 */
+		
+		return (value<=(center+range))&&(value>=(center-range));
+	}
 }
