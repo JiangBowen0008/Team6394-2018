c6477178e99ef56188c4265ec7049d73f6684356
Antiarisxu <jimmyxurs@gmail.com>
2018-07-01 12:10:14 +0800

    Update Robot.java

--- PID Drive/src/org/usfirst/frc/team6394/robot/Robot.java
+++ PID Drive/src/org/usfirst/frc/team6394/robot/Robot.java
@@ -7,50 +7,61 @@
 
 package org.usfirst.frc.team6394.robot;
 
-import com.ctre.phoenix.motorcontrol.can.*;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Spark;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import com.ctre.phoenix.motorcontrol.*;
-import org.usfirst.frc.team6394.robot.Constant.*;
-import static java.lang.Math.*;
+import edu.wpi.first.wpilibj.I2C.Port;
 
+import com.kauailabs.navx.frc.AHRS;
 
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the IterativeRobot
  * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
+ * creating this project, you must also update the build.properties file in the
+ * project.
  */
 public class Robot extends IterativeRobot {
-
-	private Joystick m_stick = new Joystick(5);
-	private Timer m_timer = new Timer();
-	private ClosedLoopDrive drive= new ClosedLoopDrive();
-	
-	
-	//SensorDifferentialBase Base=new SensorDifferentialBase(LTalon,RTalon);
-	
-	
+	private static final String kDefaultAuto = "Default";
+	private static final String kCustomAuto = "My Auto";
+	private String m_autoSelected;
+	private SendableChooser<String> m_chooser = new SendableChooser<>();
+	private ADXRS450_Gyro Gyro= new ADXRS450_Gyro();
+	private Joystick m_stick= new Joystick(5);
+	private final AHRS ahrs = new AHRS(Port.kMXP);
 
 	/**
 	 * This function is run when the robot is first started up and should be
 	 * used for any initialization code.
 	 */
 	@Override
-	public void robotInit() {	
+	public void robotInit() {
+		m_chooser.addDefault("Default Auto", kDefaultAuto);
+		m_chooser.addObject("My Auto", kCustomAuto);
+		SmartDashboard.putData("Auto choices", m_chooser);
+		Gyro.calibrate();
+		ahrs.reset();
 	}
-	
+
 	/**
-	 * This function is run once each time the robot enters autonomous mode.
+	 * This autonomous (along with the chooser code above) shows how to select
+	 * between different autonomous modes using the dashboard. The sendable
+	 * chooser code works with the Java SmartDashboard. If you prefer the
+	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+	 * getString line to get the auto name from the text box below the Gyro
+	 *
+	 * <p>You can add additional auto modes by adding additional comparisons to
+	 * the switch structure below with additional strings. If using the
+	 * SendableChooser make sure to add them to the chooser code above as well.
 	 */
 	@Override
 	public void autonomousInit() {
-		m_timer.reset();
-		m_timer.start();
+		m_autoSelected = m_chooser.getSelected();
+		// autoSelected = SmartDashboard.getString("Auto Selector",
+		// defaultAuto);
+		System.out.println("Auto selected: " + m_autoSelected);
 	}
 
 	/**
@@ -58,25 +69,32 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void autonomousPeriodic() {
-		// Drive for 2 seconds
-		if (m_timer.get() < 2.0) {
-			drive.velDrive(0.5, 0.0); // drive forwards half speed
-		} 
+		switch (m_autoSelected) {
+			case kCustomAuto:
+				// Put custom auto code here
+				break;
+			case kDefaultAuto:
+			default:
+				// Put default auto code here
+				break;
+		}
 	}
 
 	/**
-	 * This function is called once each time the robot enters teleoperated mode.
-	 */
-	@Override
-	public void teleopInit() {
-	}
-
-	/**
-	 * This function is called periodically during teleoperated mode.
+	 * This function is called periodically during operator control.
 	 */
 	@Override
 	public void teleopPeriodic() {
-		drive.velDrive(util.deadband(m_stick.getY(),0.1), util.deadband(m_stick.getX(),0.1));
+		SmartDashboard.putBoolean("Gyro_connected", Gyro.isConnected());
+		SmartDashboard.putNumber("Gyro_angle", Gyro.getAngle());
+		SmartDashboard.putNumber("ahrs_angle", ahrs.getAngle());
+		SmartDashboard.putNumber("ahrs_X", ahrs.getDisplacementX());
+		SmartDashboard.putNumber("ahrs_Y", ahrs.getDisplacementY());
+		SmartDashboard.putNumber("ahrs_Z", ahrs.getDisplacementZ());
+		if(m_stick.getRawButtonPressed(1)) {
+			Gyro.reset();
+			ahrs.reset();
+		}
 	}
 
 	/**
@@ -85,5 +103,5 @@ public class Robot extends IterativeRobot {
 	@Override
 	public void testPeriodic() {
 	}
-	
 }
+
