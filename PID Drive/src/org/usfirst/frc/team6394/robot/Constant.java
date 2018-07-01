39858402f92d53c97cfd84326ec9d0ab7bbb5a03
Antiarisxu <jimmyxurs@gmail.com>
2018-07-01 12:10:36 +0800

    Update Constant.java

--- PID Drive/src/org/usfirst/frc/team6394/robot/Constant.java
+++ PID Drive/src/org/usfirst/frc/team6394/robot/Constant.java
@@ -2,15 +2,19 @@ package org.usfirst.frc.team6394.robot;
 
 public class Constant {
 	
+	//measurement
+	
+	public final static double rotate90=1.03;
+	
 	//Pin
 	
-	public final static int LTalonID=0;
+	public final static int LTalonEID=3;
+	
+	public final static int RTalonEID=1;
+	
+	public final static int LTalonID=4;
 	
 	public final static int RTalonID=2;
-	
-	public final static int LVictorID=1;
-	
-	public final static int RVictorID=3;
 	
 	//Constants
 	
@@ -19,5 +23,9 @@ public class Constant {
 	public final static int kPIDLoopIdx = 0;
 
 	public final static int kTurnTimeoutSec = 3;
+	
+	//Adjust
+	
+	public final static double EndingSpeed=0.13;
 	
 }
